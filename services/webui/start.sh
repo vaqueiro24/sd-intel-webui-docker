e4d2d027473d3fa657a91bb7e7d1d914fb405ff2
theunpleasantowl <theunpleasantowl@gmail.com>
2023-03-12 13:13:14 -0400

    fix output always being UID 0

--- services/webui/start.sh
+++ services/webui/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
+# Execute
 source /opt/intel/oneapi/setvars.sh
-cd /stable-diffusion-webui
-python3 /stable-diffusion-webui/launch.py --use-intel-oneapi --listen --port ${WEBUI_PORT:=7860} ${CLI_ARGS}
+python3 launch.py --use-intel-oneapi --listen --port ${WEBUI_PORT:=7860} ${CLI_ARGS}
